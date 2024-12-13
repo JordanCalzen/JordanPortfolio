8e12766753a2c710e6cfa0807e4c92114b2f8b90
JordanCalzen <jordancarlzencare@gmail.com>
2024-12-13 15:43:27 -0800

    added profile pic

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,8 @@
 const minimizeBtn = document.querySelector(".minmizeBtn");
-console.log(minimizeBtn);
 const heroLeft = document.querySelector(".hero-left");
 const heroRight = document.querySelector(".hero-right");
+const modeSwitch = document.querySelector(".modeSwitch");
+console.log(modeSwitch);
 
 minimizeBtn.addEventListener("click", () => {
 	heroLeft.classList.toggle("minimal");
