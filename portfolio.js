d012d64e34540cf28aa488f02f30f5ba4fc4be54
JordanCalzen <jordancarlzencare@gmail.com>
2024-12-13 14:07:51 -0800

    added the nav bars

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,9 @@
+const minimizeBtn = document.querySelector(".minmizeBtn");
+console.log(minimizeBtn);
+const heroLeft = document.querySelector(".hero-left");
+const heroRight = document.querySelector(".hero-right");
 
+minimizeBtn.addEventListener("click", () => {
+	heroLeft.classList.toggle("minimal");
+	ReportBody.heroRight.style.paddingLeft = "5rem";
+});
